5b53a28e148790fc7701fcd5eaa39b79c5a50361
Ahmed Magdy Mostafa Ali <117994460+hagoog@users.noreply.github.com>
2023-04-21 14:39:50 +0200

    form with internal validation

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -1,18 +1,31 @@
 import React from "react";
 import "./ReservationFormStyles.css";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const ReservationForm = (props) => {
   const form = useForm();
-  const { register } = form;
-
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+  const onSubmit = (data) => {
+    console.log("Form Submitted", data);
+  };
   return (
     <main className="form-container">
       <h2>{props.title}</h2>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-element">
           <label htmlFor="branch">Select Branch *</label>
-          <select id="branch" {...register("branch")}>
+          <select
+            id="branch"
+            {...register("branch", {
+              required: { value: true, message: "Please Select a Branch" },
+            })}
+          >
             <option value="" selected disabled hidden>
               Select below
             </option>
@@ -26,7 +39,16 @@ const ReservationForm = (props) => {
           <input
             type="date"
             id="reservationDate"
-            {...register("reservationDate")}
+            {...register(
+              "reservationDate",
+              {
+                required: {
+                  value: true,
+                  message: "Please Select a reservation date",
+                },
+              },
+              { valueAsDate: true }
+            )}
           ></input>
         </div>
         <div className="form-element">
@@ -34,7 +56,12 @@ const ReservationForm = (props) => {
           <input
             type="radio"
             id="lunch"
-            {...register("time")}
+            {...register("time", {
+              required: {
+                value: true,
+                message: "Please specify dinner or lunch",
+              },
+            })}
             value="lunch"
           ></input>
           <label htmlFor="lunch">Lunch</label>
@@ -53,25 +80,56 @@ const ReservationForm = (props) => {
           <input
             type="number"
             id="numberOfGuests"
-            {...register("numberOfGuests")}
             min={1}
             max={8}
+            {...register(
+              "numberOfGuests",
+              {
+                required: {
+                  value: true,
+                  message: "please specify number of guests between 1 and 8",
+                },
+              },
+              {
+                min: { value: 1, message: "the minimum number of guests is 1" },
+              },
+              {
+                max: { value: 8, message: "the maximum number of guests is 8" },
+              }
+            )}
           ></input>
         </div>
         <div className="form-element">
           <label htmlFor="name">Full Name *</label>
-          <input type="text" id="name" {...register("name")}></input>
+          <input
+            type="text"
+            id="name"
+            {...register("name", {
+              required: { value: true, message: "please enter your full name" },
+            })}
+          ></input>
         </div>
         <div className="form-element">
           <label htmlFor="email">E-Mail *</label>
-          <input type="email" id="email" {...register("email")}></input>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              required: { value: true, message: "please enter your email" },
+            })}
+          ></input>
         </div>
         <div className="form-element">
           <label htmlFor="phoneNumber">Phone Number *</label>
           <input
             type="tel"
             id="phoneNumber"
-            {...register("phoneNumber")}
+            {...register("phoneNumber", {
+              required: {
+                value: true,
+                message: "please enter your phone number",
+              },
+            })}
           ></input>
         </div>
         <div className="form-element">
@@ -84,16 +142,28 @@ const ReservationForm = (props) => {
           ></textarea>
         </div>
         <div className="terms">
-          <input type="checkbox" id="notice" {...register("notice")}></input>
+          <input
+            type="checkbox"
+            id="notice"
+            {...register("notice", {
+              required: {
+                value: true,
+                message: "please review & approve our policy and terms",
+              },
+            })}
+          ></input>
           <label htmlFor="notice">
             <b>I agree with the terms and conditions and privacy policy.</b>
           </label>
         </div>
         <div className="reserve">
-          <button className="reserve-btn">Reserve a Tabel</button>
+          <button className="reserve-btn" type="submit">
+            Reserve a Table
+          </button>
           <button className="cancel-btn">Cancel</button>
         </div>
       </form>
+      <DevTool control={control} />
     </main>
   );
 };
